eedaa3f519287ff84726c16b90823011aa28c204
Pedro Scott <scott7462@gmail.com>
2016-04-18 07:10:46 -0500

    Widget
    Type: Refactor.
    Body: Add the new AppWidgetProvider.

---
+++ StockHawk-student_release/app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by scott7462 on 4/17/16.
+ */
+public class QuoteWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
