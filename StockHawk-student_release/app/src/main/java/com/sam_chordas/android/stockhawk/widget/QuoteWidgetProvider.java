3b0bf437118dfc43d9149ce76be278ec97fe51b9
Pedro Scott <scott7462@gmail.com>
2016-04-19 07:11:28 -0500

    Type: Refactor. Body: Work in the widget to et final widget.

--- StockHawk-student_release/app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
+++ StockHawk-student_release/app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -3,6 +3,11 @@ package com.sam_chordas.android.stockhawk.widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.sam_chordas.android.stockhawk.R;
 
 /**
  * Created by scott7462 on 4/17/16.
@@ -12,5 +17,9 @@ public class QuoteWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_collection);
+        // find your TextView here by id here and update it.
+
     }
 }
