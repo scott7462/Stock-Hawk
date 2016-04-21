766105672ccdd8f9f58146b409ca6f35b9714f24
Pedro Scott <scott7462@gmail.com>
2016-04-21 07:04:49 -0500

    Events
    Update events to add the tag and show a message.

--- StockHawk-student_release/app/src/main/java/com/sam_chordas/android/stockhawk/app/busevents/events/EventUpdateData.java
+++ StockHawk-student_release/app/src/main/java/com/sam_chordas/android/stockhawk/app/busevents/events/EventUpdateData.java
@@ -5,12 +5,22 @@ package com.sam_chordas.android.stockhawk.app.busevents.events;
  */
 public class EventUpdateData {
     private final int result;
+    private String tag;
 
-    public EventUpdateData(int result) {
+    public EventUpdateData(int result, String tag) {
         this.result = result;
+        this.tag = tag;
     }
 
     public int getResult() {
         return result;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 }
